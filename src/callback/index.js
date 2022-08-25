4101baf7c913e8b7908504844ca5f1c981e2b4a8
David Duarte <dhduartea@gmail.com>
2022-08-25 16:19:54 -0500

    Introducción de callbacks

---
+++ src/callback/index.js
@@ -0,0 +1,17 @@
+function sum(num1, num2) {
+    return num1+num2;
+}
+
+function calc(num1, num2, callback) {
+    return callback(num1,num2);
+}
+console.log(calc(1, 2,sum));
+
+setTimeout(() => {
+    console.log("hola Js");
+}, 2000);
+
+function gretting(name) {
+    console.log(`Hola ${name}`);
+}
+setTimeout(gretting, 5000, ("David") );
